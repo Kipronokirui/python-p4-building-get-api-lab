5ae32454ed7b6031b2c831962f31603f541a46de
Kipronokirui <victorkipronokirui2@gmail.com>
2024-04-06 21:45:52 +0300

    Tests passed

--- server/app.py
+++ server/app.py
@@ -20,19 +20,34 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = []
+    for bakery in Bakery.query.all():
+        bakeries.append(bakery.to_dict())
+    
+    response = make_response(bakeries, 200)
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(id=id).first()
+    response = make_response(bakery.to_dict(), 200)
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods_by_price = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_by_price_serialized = []
+    for baked_good in baked_goods_by_price:
+        baked_goods_by_price_serialized.append(baked_good.to_dict())
+    
+    response = make_response(baked_goods_by_price_serialized, 200)
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
+    response = make_response(most_expensive.to_dict(), 200)
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
